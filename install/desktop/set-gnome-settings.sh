9618537fa3ff92051a8ad30e60320365039d206f
Christopher Sale <christopher.sale@gmail.com>
2024-09-24 08:15:08 +0930

    set workspaces to all monitors

--- install/desktop/set-gnome-settings.sh
+++ install/desktop/set-gnome-settings.sh
@@ -1,6 +1,9 @@
 # Center new windows in the middle of the screen
 gsettings set org.gnome.mutter center-new-windows true
 
+# Workspaces apply to all additional monitors, not just primary
+gsettings set org.gnome.mutter workspaces-only-on-primary false
+
 # Set Cascadia Mono as the default monospace font
 gsettings set org.gnome.desktop.interface monospace-font-name 'CaskaydiaMono Nerd Font 10'
 
